39ecec75529a1b85767fb0d01633e651af82d5b2
Varun-6390 <artistvarunlko@gmail.com>
2025-03-25 22:14:54 +0530

    Checking Even or Odd

---
+++ Program_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+    int num;
+
+    // Input From the User
+
+    printf("Enter a number : ");
+    scanf("%d", &num);
+
+    // Checking Number
+
+    if (num % 2 == 0)
+    {
+        printf("Number is  even.");
+    }
+    else
+    {
+        printf("Number is odd.");
+    }
+}
