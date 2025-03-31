293123bd6883ca2858119b3da0199546148451f0
Varun-6390 <artistvarunlko@gmail.com>
2025-03-31 21:57:21 +0530

    Reverse of a number

---
+++ Program_9.c
@@ -0,0 +1,18 @@
+// Reverse of a number
+
+#include <stdio.h>
+void main()
+{
+    int n, rev = 0, rem;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    printf("Reversed Number : %d", rev);
+}
