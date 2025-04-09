ef5d372e871ae612e317ae8a8ec6266a6ece20e2
Varun-6390 <artistvarunlko@gmail.com>
2025-04-09 22:06:03 +0530

    Bill calculator

---
+++ Program_27.c
@@ -0,0 +1,19 @@
+// Bill calculator
+
+#include <stdio.h>
+
+int main()
+{
+    float price, total = 0;
+    int i, items;
+    printf("Enter number of items: ");
+    scanf("%d", &items);
+    for (i = 1; i <= items; i++)
+    {
+        printf("Enter price of %d item: ", i);
+        scanf("%f", &price);
+        total = total + price;
+    }
+    printf("Total bill: Rs.%.2f\n", total);
+    return 0;
+}
