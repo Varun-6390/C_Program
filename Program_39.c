0368b2dc42ef00e5e023e923163b40cfa477e197
Varun-6390 <artistvarunlko@gmail.com>
2025-04-15 22:06:00 +0530

    negative, positive or Zero

---
+++ Program_39.c
@@ -0,0 +1,23 @@
+// Check the number is negative, positive or Zero
+
+#include <stdio.h>
+void main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n > 0)
+    {
+        printf("The number is positive");
+    }
+    else if (n < 0)
+    {
+        printf("The number is negative");
+    }
+    else
+    {
+        printf("The number is zero");
+    }
+}
